6551421d618ceb7e8d2db14ae028c752652946e3
mcdowell84 <68907957+mcdowell84@users.noreply.github.com>
2021-12-07 18:29:34 +0100

    crabby

--- 06b.py
+++ 06b.py
@@ -16,7 +16,6 @@ for count, value in enumerate(input):
 
 def newtimer(fishdict):
 	for i in range(0,9):
-		#fishdict[-1] = 0
 		fishdict[i-1] = fishdict[i]
 	#print(fishdict)
 	newborn = fishdict[-1]
@@ -24,23 +23,11 @@ def newtimer(fishdict):
 	del fishdict[-1]
 	fishdict[8] = newborn
 	return fishdict
-
-def timer(input):
-	newborn = 0
-	for count, value in enumerate(input):
-		if input[count] == 0:
-			newborn += 1
-		input[count] -= 1
-		if input[count] < 0:
-			input[count] = 6
-	for i in range(newborn):
-		input.append(8)
-	return input	
+	
 days = 256
 
 for i in range(days):
 	fishdict = newtimer(fishdict)			
-#print(fishdict)
 	
 values = fishdict.values()
 total = sum(values)
---
+++ 07a.py
@@ -0,0 +1,58 @@
+import operator
+
+input = open('input/07.txt', 'r')
+input = input.read().split(',')
+input = list(map(int, input))
+
+testinput = [16,1,2,0,4,2,7,1,2,14]
+
+#input = testinput
+input.sort(reverse=True)
+# print(input)
+crabdict = {}
+#for i in range():
+#	fishdict[i] = 0
+
+for count, value in enumerate(input):
+	crabdict[value] = 0
+
+for count, value in enumerate(input):
+	crabdict[value] += 1
+
+sorted_d = sorted(crabdict.items(), key=operator.itemgetter(1), reverse=True)
+#print(sorted_d)
+
+total = 0
+
+
+
+def fuelcalc(goal):
+	fuel = 0
+	for o in range(number):
+		tuple = sorted_d[o]
+		pos = tuple[0]
+		multi = tuple[1]
+		fuel += abs(pos - goal)*multi
+	return fuel
+		
+ 
+
+number = len(sorted_d)
+
+best = 10000000000000000000
+
+for i in range(number):
+	total = 0
+	value = sorted_d[i]
+	goal = value[0]
+	total = fuelcalc(goal)
+	if total < best:
+		best = total
+		
+print(best)
+	
+	
+	
+
+
+	
